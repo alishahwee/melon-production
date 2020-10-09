6eb734235dcd78a88e682e45e0cdbd497e7d4959
alishahwee <alisha.hwee@outlook.com>
2020-10-08 20:50:58 -0500

    Implement a Squash class with painterbot submodule

--- melons.py
+++ melons.py
@@ -33,4 +33,32 @@ class Melon(object):
 
 # FIXME: Add Squash class definition here.
 class Squash(Melon):
-    pass
+    """Squash."""
+
+    def __init__(self, melon_type):
+        """Initialize squash.
+
+        melon_type: type of melon to be built into a squash.
+        """
+
+        self.melon_type = melon_type
+        self.weight = 0.0
+        self.color = None
+        self.stickers = []
+
+    def prep(self):
+        """Prepare the melon."""
+
+        robots.cleanerbot.clean(self)
+        robots.stickerbot.apply_logo(self)
+        robots.painterbot.paint(self)
+
+    def __str__(self):
+        """Print out information about melon."""
+
+        if self.weight <= 0:
+            return self.melon_type
+        else:
+            return "{} {:.2f} lbs {}".format(self.color,
+                                             self.weight,
+                                             self.melon_type)
